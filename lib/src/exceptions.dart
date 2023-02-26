05bf108b0883797fc6a594e58a43d2b120b16e0a
Daniel Weiss <daniel.weiss.de@gmail.com>
2023-02-26 21:07:33 +0100

    more descriptive error message when wrapper not found

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -2,6 +2,9 @@ part of "../customized_keyboard.dart";
 
 class KeyboardNotRegisteredError implements Exception {}
 
-class KeyboardWrapperNotFound implements Exception {}
+class KeyboardWrapperNotFound implements Exception {
+  final String message = "[CustomTextFormField] and [CustomTextField] need to be wrapped "
+      "in a [KeyboardWrapper].";
+}
 
 class KeyboardMissingConnection implements Exception {}
