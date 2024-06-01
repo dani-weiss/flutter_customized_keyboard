c22a4bb534b67b2ea9535f65c6e4cb1fe3fa9acf
Daniel Weiss <daniel.weiss.de@gmail.com>
2024-06-01 22:59:58 +0200

    Visibility calculations and ensuring item is visible closest to its current position

--- lib/customized_keyboard.dart
+++ lib/customized_keyboard.dart
@@ -1,5 +1,6 @@
 library customized_keyboard;
 
+import 'package:customized_keyboard/src/visibility.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:flutter/gestures.dart';
